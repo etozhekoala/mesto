09b5d04f5f690bfff33e609c8962ae831faa9121
Anatoly <etozhekoala@gmail.com>
2023-03-26 19:37:17 +0300

    fix: Изменить файл script.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,29 +10,32 @@ const btnClose = popup.querySelector('.popup__button-close');
 const authorName = document.querySelector('.profile__author-name');
 const authorJob = document.querySelector('.profile__author-description');
 
-btnOpen.addEventListener('click', () => {
-  
-  popup.classList.add('popup_opened');
-
-  nameInput.value = authorName.textContent;
-  jobInput.value = authorJob.textContent;
-});
-
-btnClose.addEventListener('click', () => {
-  
-  popup.classList.remove('popup_opened');
-
-});
 
 function handleFormSubmit (event) {
-
+  
   event.preventDefault();
   
   authorName.textContent = nameInput.value;
   authorJob.textContent = jobInput.value;
   
   popup.classList.remove('popup_opened');
+  
+}
+
+function openForm() {
+  popup.classList.add('popup_opened');
+  
+  nameInput.value = authorName.textContent;
+  jobInput.value = authorJob.textContent;
+  
+}
+
+function closeForm() {
+
+  popup.classList.remove('popup_opened');
 
 }
 
+btnOpen.addEventListener('click', openForm);
+btnClose.addEventListener('click', closeForm);
 popupForm.addEventListener('submit', handleFormSubmit);
