fe764b08a023f88d8df67d2b1eda8b6fd820193d
Anatoly <etozhekoala@gmail.com>
2023-04-23 13:11:03 +0700

    fix: Перенос карточек в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -102,35 +102,6 @@ popupForm.addEventListener('submit', handleFormSubmit);
 // btnClose.addEventListener('click', closeForm);
 // popupForm.addEventListener('submit', handleFormSubmit);
 
-
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-]; 
-
 const templateItem = document.querySelector('.template__item');
 const cardContainer = document.querySelector('.elements__items');
 
