37a509b7139572ed576769c697bea82bb3299bd9
Anatoly <etozhekoala@gmail.com>
2023-04-25 05:47:02 +0700

    fix: Поправить BEM

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const templateItem = document.querySelector('.elements__template-item');
 
 const editProfileButtonOpen = document.querySelector('.profile__edit-button');
 const editProfilePopup = document.querySelector('.popup_type_edit-profile');
-const editProfileButtonClose = editProfilePopup.querySelector('.button-close_type_edit-popup');
+const editProfileButtonClose = editProfilePopup.querySelector('.popup__button-close_type_edit-popup');
 const editProfileForm = editProfilePopup.querySelector('.popup__form_type_profile-edit');
 const editProfileNameInput = editProfilePopup.querySelector('.popup__form-input_type_name');
 const editProfileJobInput = editProfilePopup.querySelector('.popup__form-input_type_job');
@@ -13,13 +13,13 @@ const editProfileAuthorJob = profileInfo.querySelector('.profile__author-descrip
 
 const addCardButtonOpen = document.querySelector('.profile__add-button');
 const addCardPopup = document.querySelector('.popup_type_add-card');
-const addCardButtonClose = addCardPopup.querySelector('.button-close_type_add-card');
+const addCardButtonClose = addCardPopup.querySelector('.popup__button-close_type_add-card');
 const addCardEditForm = addCardPopup.querySelector('.popup__form_type_add-card');
 const addCardTitleInput = addCardEditForm.querySelector('.popup__form-input_type_title');
 const addCardLinkInput = addCardEditForm.querySelector('.popup__form-input_type_link');
 
 const fullscreenPopup = document.querySelector('.popup_type_fullscreen');
-const fullscreenCloseButton = fullscreenPopup.querySelector('.popup__button-close');
+const fullscreenCloseButton = fullscreenPopup.querySelector('.popup__button-close_type_fullscreen');
 const fullscreenImage = fullscreenPopup.querySelector('.popup__image-fullscreen');
 const fullscreenTitleCard = fullscreenPopup.querySelector('.popup__fullscreen-title');
 
@@ -45,9 +45,11 @@ const createCardElement = (cardData) => {
   const handleDelete = () => {
     cardElement.remove();
   };
+
   deleteBtn.addEventListener('click', handleDelete);
 
   const likeBtn = cardElement.querySelector('.elements__like-button');
+
   const handleLike = () => {
     likeBtn.classList.toggle('elements__like-button_active');
   };
@@ -55,15 +57,16 @@ const createCardElement = (cardData) => {
   likeBtn.addEventListener('click', handleLike);
 
   const handleFullScreen = () => {
-    
+
     fullscreenImage.src = cardImage.src;
     fullscreenImage.alt = cardName.textContent;
     fullscreenTitleCard.textContent = cardName.textContent;
+
     openPopup(fullscreenPopup);
   };
 
   cardImage.addEventListener('click', handleFullScreen);
-  
+
   return cardElement;
 };
 
@@ -71,7 +74,7 @@ const renderCardElement = (cardElement) => {
   cardContainer.append(cardElement);
 };
 
-initialCards.forEach((card) => { 
+initialCards.forEach((card) => {
   renderCardElement(createCardElement(card));
 });
 
@@ -79,7 +82,7 @@ const handleAddCardFormSubmit = (event) => {
   event.preventDefault();
   const name = addCardTitleInput.value;
   const link = addCardLinkInput.value;
-  
+
   const newCardData = {
     name, link,
 };
@@ -91,15 +94,15 @@ cardContainer.prepend(createCardElement(newCardData));
 
 function openProfileEditForm() {
   openPopup(editProfilePopup);
-  
+
   editProfileNameInput.value = editProfileAuthorName.textContent;
   editProfileJobInput.value = editProfileAuthorJob.textContent;
 };
 
 function handleProfileFormSubmit (event) {
-  
+
   event.preventDefault();
-  
+
   editProfileAuthorName.textContent = editProfileNameInput.value;
   editProfileAuthorJob.textContent = editProfileJobInput.value;
 
@@ -129,4 +132,3 @@ addCardEditForm.addEventListener('submit', handleAddCardFormSubmit);
 fullscreenCloseButton.addEventListener('click', () => {
   closePopup(fullscreenPopup);
 });
-
